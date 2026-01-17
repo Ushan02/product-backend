95b824a1101555be194af8e7fbc5489b737becef
Ushan02 <ushansanuka2023@gmail.com>
2026-01-17 07:40:43 +0530

    getProduct

--- routes/productrouter.js
+++ routes/productrouter.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { deleteProduct, getProduct, saveProduct, updateProduct } from "../Controller/productCont.js";
+import { deleteProduct, getProduct, getProductById, saveProduct, updateProduct } from "../Controller/productCont.js";
 
 
 const productRouter = express.Router()
 
 productRouter.get("/",getProduct)
-
 productRouter.post("/",saveProduct)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
+productRouter.get("/:productId",getProductById)
 
 
 export default productRouter;
